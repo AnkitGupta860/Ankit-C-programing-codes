887a30411cf0135639396170ef79410766c7ded3
Ankit Gupta <ankit.gupta_cs.aiml20@gla.ac.in>
2021-07-31 20:16:06 +0530

    stack implementation

---
+++ .vscode/stack_implementation.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+int isEmpty(struct stack * ptr)
+{
+    if(ptr->top < 0 )
+        return 1;
+    return 0;    
+}
+int isFull(struct stack * ptr)
+{
+    if(ptr->top == ptr->size-1)
+        return 1;
+    return 0;    
+}
+int main()
+{
+    struct stack *s;
+    s->size = 80;
+    s->top = -1;
+    s->arr = (int *)malloc(s->size * sizeof(int));
+    s->arr[0] = 5;
+    s->top++;
+    if(isEmpty(s))
+        printf("stack is empty\n");
+    else
+        printf("stack is not empty\n");
+}
+
